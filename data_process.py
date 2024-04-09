67299b053d5cf52b544a50a024510c6e041f2f82
Keith1904 <keithfernandes1904@gmail.com>
2024-04-08 21:57:43 -0400

    pressure case

---
+++ data_process.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 21:04:47 2024
+
+@author: Keith
+
+
+
+"""
+from scipy.io import loadmat, savemat
+import pandas as pd
+import numpy as np
+
+
+def load_exp_data(exp_df):
+    data_d = loadmat(exp_df)
+    fs = 500
+    p_array = data_d["Wind_pressure_coefficients"]
+    exp_time_array = np.arange(p_array.shape[0]) / fs
+    location_array = data_d["Location_of_measured_points"]
+    location_df = pd.DataFrame(location_array.T, columns=["x", "y", "tap_number", "face_number"])
+    return p_array, location_df, exp_time_array
+
+
+p_array, location_df, exp_time_array = load_exp_data(r"C:\Users\Keith\Documents\research_paper\pressure-case\Data\time_series_of_point_wind_pressure_0.mat")
+
+p_array = pd.DataFrame(p_array)
+p_array.insert(0, 'Time', exp_time_array)
+p_array.columns = ['Time'] + [str(i) for i in range(len(p_array.columns) - 1)]
+
+p_array.to_csv(r'C:\Users\Keith\Documents\research_paper\pressure-case\Data\p.csv', index=False)
+
+location_df.insert(0, 'Index', range(len(location_df)))
+location_df.to_csv(r'C:\Users\Keith\Documents\research_paper\pressure-case\Data\coords.csv', index=False)
